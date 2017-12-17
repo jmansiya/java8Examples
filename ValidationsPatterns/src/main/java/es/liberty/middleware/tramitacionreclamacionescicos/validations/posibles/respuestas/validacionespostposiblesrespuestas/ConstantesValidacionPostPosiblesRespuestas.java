f1cb7317ce7d8efdcfe48f28c0cc26b2edbc7bd6
Jose Mansilla <jmansiya@users.noreply.github.com>
2017-12-17 20:36:55 +0100

    La comprobación de la lista de mensajes con el código del mensaje no era correcta.

--- ValidationsPatterns/src/main/java/es/liberty/middleware/tramitacionreclamacionescicos/validations/posibles/respuestas/validacionespostposiblesrespuestas/ConstantesValidacionPostPosiblesRespuestas.java
+++ ValidationsPatterns/src/main/java/es/liberty/middleware/tramitacionreclamacionescicos/validations/posibles/respuestas/validacionespostposiblesrespuestas/ConstantesValidacionPostPosiblesRespuestas.java
@@ -1,10 +1,11 @@
 package es.liberty.middleware.tramitacionreclamacionescicos.validations.posibles.respuestas.validacionespostposiblesrespuestas;
 
 import es.liberty.middleware.tramitacionreclamacionescicos.model.reclamacionescicos.MensajeDialogoCicos;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 import java.util.function.BiPredicate;
 
 public class ConstantesValidacionPostPosiblesRespuestas {
-    public static final BiPredicate<List<MensajeDialogoCicos>, String> existeMensajeEnLaLista = (listaMensajes, codMensaje) -> listaMensajes.contains(codMensaje);
+    public static final BiPredicate<List<MensajeDialogoCicos>, String> existeMensajeEnLaLista = (listaMensajes, codMensaje) -> listaMensajes.stream().anyMatch(e -> StringUtils.equalsIgnoreCase(e.getKey(), codMensaje));
 }
